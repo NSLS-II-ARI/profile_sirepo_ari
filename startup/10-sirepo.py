eb0a364c29a86dbf4bd81944090b3f20bc9e30cc
Maksim Rakitin <mrakitin@bnl.gov>
2022-11-07 20:54:52 -0500

    Update `README.md` and add data files

--- startup/10-sirepo.py
+++ startup/10-sirepo.py
@@ -2,7 +2,7 @@ from sirepo_bluesky.sirepo_bluesky import SirepoBluesky
 from sirepo_bluesky.sirepo_ophyd import create_classes
 
 # Assumption: there is a running local instance of Sirepo. Please follow the
-# instructions at http://nsls-ii.github.io/sirepo-bluesky/installation.html to
+# instructions at https://nsls-ii.github.io/sirepo-bluesky/installation.html to
 # install/configure Sirepo and Sirepo-Bluesky.
 connection = SirepoBluesky("http://localhost:8000")
 
